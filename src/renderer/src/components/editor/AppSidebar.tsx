b968b8d6e280d1e34738c49797d8a3b9ddad3585
Sacha Sayan <sacha.sayan@gmail.com>
2025-02-15 02:02:18 -0500

    Fix sidebar.

--- src/renderer/src/components/editor/AppSidebar.tsx
+++ src/renderer/src/components/editor/AppSidebar.tsx
@@ -14,6 +14,7 @@ import {
 import { Plus, Save, X, Diff, LayoutDashboard, Settings, FileText, ListOrdered, Squircle } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
+  useSidebar,
   Sidebar,
   SidebarContent,
   SidebarGroup,
@@ -36,7 +37,6 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger
 } from '@/components/ui/alert-dialog'
-import { addChatMessage } from '@/lib/utils/chatSlice'
 import { selectAppState } from '@/lib/utils/appStateSlice'
 
 const ChapterIcon = ({ chapterNumber }: { chapterNumber: string | number }) => {
@@ -54,6 +54,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const dispatch = useDispatch()
   const projectsState = useSelector(selectProjects)
   const appState = useSelector(selectAppState)
+  const [alertDialogOpen, setAlertDialogOpen] = React.useState(false);
+  const { open: sideBarOpen, setOpen: setSideBarOpen } = useSidebar()
+
+
 
   const handleFileSelect = (fileName: string) => {
     dispatch(setActiveFile(fileName))
@@ -62,153 +66,167 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
 
 
+
+  const handleClose = () => {
+    dispatch(setProjectHasLiveEdits(false))
+    dispatch(setActiveProject(null))
+    dispatch(setActiveView('intro'))
+  }
   const handleSave = async () => {
     if (projectsState.activeProject) {
       const saveResult = await saveProject(projectsState.activeProject)
       if (saveResult) {
         toast.success('Project saved successfully!')
         dispatch(setAllFilesAsSaved())
+        return true;
       } else {
         toast.error(`Failed to save project: ${saveResult}`)
+        return false;
       }
       dispatch(setProjectHasLiveEdits(false))
     }
   }
+  const saveAndClose = async () => {
+    const result = await handleSave();
+    if (result) {
+      handleClose()
+    }
+  }
+  const handleCloseSafe = async () => {
+    if (projectsState.projectHasLiveEdits) {
+      setAlertDialogOpen(true);
+      return false;
+    }
+    handleClose()
+  }
+
+
 
   return (
-    <Sidebar variant="sidebar" collapsible="icon" {...props}>
-      <SidebarHeader>
-        <div className="grid grid-cols-3 gap-2">
-          {projectsState.projectHasLiveEdits ? (
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button variant="outline" className="">
-                  <X className="" /> Close
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    You have unsaved changes. Closing will lose your progress.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <AlertDialogAction
-                    onClick={() => {
-                      dispatch(setProjectHasLiveEdits(false))
-                      dispatch(setActiveProject(null))
-                      dispatch(setActiveView('intro'))
-                    }}
-                  >
-                    Close without Saving
-                  </AlertDialogAction>
-                  <AlertDialogAction
-                    onClick={async () => {
-                      await handleSave()
-                      dispatch(setProjectHasLiveEdits(false))
-                      dispatch(setActiveProject(null))
-                      dispatch(setActiveView('intro'))
-                    }}
-                  >
-                    Save and Close
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          ) : (
-            <Button variant="outline" className="" onClick={() => dispatch(setActiveView('intro'))}>
-              <X className="" /> Close
+    <>
+      <AlertDialog open={alertDialogOpen} onOpenChange={setAlertDialogOpen} >
+        <AlertDialogTrigger asChild>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              You have unsaved changes. Closing will lose your progress.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={handleClose}
+            >
+              Close without Saving
+            </AlertDialogAction>
+            <AlertDialogAction
+              onClick={saveAndClose}
+            >
+              Save and Close
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <Sidebar variant="sidebar" collapsible="icon"  {...props}>
+        <SidebarHeader>
+          <div className={`flex justify-between ${sideBarOpen ? `flex-row` : `flex-col`}`}>
+            <Button
+              variant="ghost"
+              className="flex-grow transition-all"
+              onClick={handleCloseSafe}
+            >
+              <X className="" /> {sideBarOpen ? 'Close' : ''}
             </Button>
-          )}
-          <Button
-            variant="outline"
-            className="col-span-2"
-            disabled={!projectsState.projectHasLiveEdits}
-            onClick={handleSave}
-          >
-            <Save className="" />
-            Save
-          </Button>
-        </div>
-      </SidebarHeader>
-      <SidebarContent className="gap-0">
-        <SidebarGroup key="Dashboard">
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem key="Dashboard">
-                <SidebarMenuButton asChild isActive={appState.activeView === 'project/dashboard'}>
-                  <a onClick={() => dispatch(setActiveView('project/dashboard'))}>
-                    <LayoutDashboard className="mr-2 h-4 w-4" /> Dashboard
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+            <Button
+              variant="ghost"
+              className="flex-grow transition-all"
+              disabled={!projectsState.projectHasLiveEdits}
+              onClick={handleSave}
+            >
+              <Save className="" />{sideBarOpen ? 'Save' : ''}
+            </Button>
+          </div>
+        </SidebarHeader>
+        <SidebarContent className="gap-0">
+          <SidebarGroup key="Dashboard">
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem key="Dashboard">
+                  <SidebarMenuButton asChild isActive={appState.activeView === 'project/dashboard'}>
+                    <a onClick={() => dispatch(setActiveView('project/dashboard'))}>
+                      <LayoutDashboard className="mr-2 h-4 w-4" /> Dashboard
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
 
-        <SidebarGroup key={"Structure"}>
-          <SidebarGroupLabel>Structure</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem key="Parameters">
-                <SidebarMenuButton asChild isActive={false}>
-                  <a onClick={() => dispatch(setActiveView('project/dashboard'))}>
-                    <Settings className="mr-2 h-4 w-4" /> Parameters
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem key="Skeleton">
-                <SidebarMenuButton asChild isActive={appState.activeView === 'project/editor' && appState.activeFile === 'Skeleton.md'}>
-                  <a onClick={() => handleFileSelect("Skeleton.md")}>
-                    <FileText className="mr-2 h-4 w-4" /> Skeleton
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem key="Outline">
-                <SidebarMenuButton asChild isActive={appState.activeView === 'project/editor' && appState.activeFile === 'Outline.md'}>
-                  <a onClick={() => handleFileSelect("Outline.md")}>
-                    <ListOrdered className="mr-2 h-4 w-4" /> Outline
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+          <SidebarGroup key={"Structure"}>
+            <SidebarGroupLabel>Structure</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                <SidebarMenuItem key="Parameters">
+                  <SidebarMenuButton asChild isActive={false}>
+                    <a onClick={() => dispatch(setActiveView('project/dashboard'))}>
+                      <Settings className="mr-2 h-4 w-4" /> Parameters
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem key="Skeleton">
+                  <SidebarMenuButton asChild isActive={appState.activeView === 'project/editor' && appState.activeFile === 'Skeleton.md'}>
+                    <a onClick={() => handleFileSelect("Skeleton.md")}>
+                      <FileText className="mr-2 h-4 w-4" /> Skeleton
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem key="Outline">
+                  <SidebarMenuButton asChild isActive={appState.activeView === 'project/editor' && appState.activeFile === 'Outline.md'}>
+                    <a onClick={() => handleFileSelect("Outline.md")}>
+                      <ListOrdered className="mr-2 h-4 w-4" /> Outline
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
 
 
-        <SidebarGroup key="Chapters">
-          <SidebarGroupLabel>Chapters</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {projectsState.activeProject?.files?.filter((item) => item.title.includes('Chapter')).map((item) => {
-                const chapterNumber = item.title.match(/Chapter-(\d+)/)?.[1] || '';
-                return (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={false}>
-                      <a onClick={() => handleFileSelect(item.title)} className="flex items-center">
-                        <ChapterIcon chapterNumber={chapterNumber} />
-                        <span className="flex-grow ml-2">{item.title.replace('.md', '').replace('-', ' ')}</span> {item.hasEdits && <Diff className="float-right text-orange-500" />}
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                );
-              })}
-              <SidebarMenuItem key="addChapter">
-                <SidebarMenuButton asChild isActive={true}>
-                  <a onClick={() => handleFileSelect("Outline.md")}><Plus /> Add Chapter</a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+          <SidebarGroup key="Chapters">
+            <SidebarGroupLabel>Chapters</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {projectsState.activeProject?.files?.filter((item) => item.title.includes('Chapter')).map((item) => {
+                  const chapterNumber = item.title.match(/Chapter-(\d+)/)?.[1] || '';
+                  return (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild isActive={false}>
+                        <a onClick={() => handleFileSelect(item.title)} className="flex items-center">
+                          <ChapterIcon chapterNumber={chapterNumber} />
+                          <span className="flex-grow ml-2">{item.title.replace('.md', '').replace('-', ' ')}</span> {item.hasEdits && <Diff className="float-right text-orange-500" />}
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  );
+                })}
+                <SidebarMenuItem key="addChapter">
+                  <SidebarMenuButton asChild isActive={true}>
+                    <a onClick={() => handleFileSelect("Outline.md")}><Plus /> Add Chapter</a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
 
 
 
 
-      </SidebarContent>
-      <SidebarRail />
-    </Sidebar>
+        </SidebarContent>
+        <SidebarRail />
+      </Sidebar>
+    </>
   )
 }
