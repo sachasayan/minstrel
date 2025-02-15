dfedb4738f725c24b4dec2d42da8d17705d5a9f7
Sacha Sayan <sacha.sayan@gmail.com>
2025-02-14 20:08:51 -0500

    Fix promptBuilder.

--- src/renderer/src/lib/promptBuilder.ts
+++ src/renderer/src/lib/promptBuilder.ts
@@ -1,37 +1,15 @@
 import { prompts } from './prompts'
 import { store } from '@/lib/utils/store'
 
-// A list of files that are required for a given file
-const getDependencyList = (fileName: string, availableFiles: string[]): string[] => {
-  const dependencies: string[] = []
-  if (fileName === 'Outline.md') {
-    dependencies.push('Skeleton.md')
-  }
-  if (fileName.startsWith('Chapter')) {
-    dependencies.push('Outline.md')
-    // Find the previous chapter.  Assume file naming convention of "Chapter-X.md", pluck all the numbers
-    const chapterNumber = fileName.match(/\d+/)
-    if (!!chapterNumber && parseInt(chapterNumber[0], 10) > 1) {
-      dependencies.push(`Chapter-${parseInt(chapterNumber[0], 10) - 1}.md`)
-    }
-  }
-  if (fileName === 'Critique.md') {
-    return availableFiles.filter((file) => file.includes('Chapter'))
-  }
 
-  return dependencies
-}
 
 // Gets all available files
 export const getAvailableFiles = (): string[] => {
   return store.getState().projects.activeProject?.files.map((f) => f.title) || []
 }
 
-// Gets just dependencies for the given file, as content
-export const getFileContents = (fileName): string => {
-  // Get the dependencies for the given file, ie [Skeleton.md, Outline.md, Chapter-1.md]
-  const dependencies = getDependencyList(fileName, getAvailableFiles())
-
+// Gets contents of all the given files as a string
+export const getFileContents = (dependencies: string[]): string => {
   // Get each file as a context item
   const activeProject = store.getState().projects.activeProject
   const context: string =
@@ -78,11 +56,8 @@ export const buildPrompt = (requestedFiles?: string[] | null): string => {
     prompt += prompts.getContext(getFileContents(requestedFiles))
   }
 
-  // if (chatHistory) {
-  //   prompt += prompts.getChatHistory(chatHistory);
-  // }
+  // Add the user message to the prompt
+  prompt += prompts.getUserPrompt(getLatestUserMesage());
 
-  console.log(prompt)
-
-  return prompt
-}
+  return prompt;
+};
