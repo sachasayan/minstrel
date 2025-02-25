eb898c9f4e94df805207703b465bc6caace7ee26
Sacha Sayan <sacha.sayan@gmail.com>
2025-02-25 15:55:53 -0500

    Nlp improvement.

--- src/renderer/src/lib/nlpUtils.ts
+++ src/renderer/src/lib/nlpUtils.ts
@@ -1,13 +1,17 @@
 import { ProjectFile } from '../types'
 
 export const stringToProjectFile = (markdownString: string): ProjectFile => {
-  const lines = markdownString.split('\\n')
-  let title = 'Untitled'
+  const lines = markdownString.trim().split('\\n')
+  let title = '(No title)';
   let content = markdownString
   for (const line of lines) {
-    if (line.startsWith('#')) {
+    if (line.startsWith('----')){
+      title = line.replace(/----/,'')
+    } else if (line.startsWith('#')) {
       title = line.replace(/^#+/, '').trim() // Remove leading #s and trim
-      content = lines.slice(lines.indexOf(line) + 1).join('\\n').trim()
+      content = lines.slice(lines.indexOf(line) + 1).join('\n').trim()
+      break
+    } else if (line.trim().length > 0 ){
       break
     }
   }
